8ca926d0691afb09d8bb1a7250c14f8c40bf0c64
hakua <hakua411@gmail.com>
2026-08-06 22:20:31 +0900

    feat: TodoCRUD実装 #3

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,8 @@
 </div>
 
 <div class="todo__content">
-  <form class="create-form">
+  <form class="create-form" action="/todos" method="post">
+  @csrf
     <div class="create-form__item">
       <input class="create-form__item-input" type="text" name="content">
     </div>
@@ -25,11 +26,15 @@
       <tr class="todo-table__row">
         <th class="todo-table__header">Todo</th>
       </tr>
+      @foreach ($todos as $todo)
       <tr class="todo-table__row">
         <td class="todo-table__item">
-          <form class="update-form">
+          <form class="update-form" action="/todos/update" method="POST">
+            @method('PATCH')
+            @csrf
             <div class="update-form__item">
-              <input class="update-form__item-input" type="text" name="content" value="test">
+              <input class="update-form__item-input" type="text" name="content" value="{{ $todo['content'] }}">
+              <input type="hidden" name="id" value="{{ $todo['id'] }}">
             </div>
             <div class="update-form__button">
               <button class="update-form__button-submit" type="submit">更新</button>
@@ -37,32 +42,17 @@
           </form>
         </td>
         <td class="todo-table__item">
-          <form class="delete-form">
+          <form class="delete-form" action="/todos/delete" method="POST">
+            @method('DELETE')
+            @csrf
             <div class="delete-form__button">
+              <input type="hidden" name="id" value="{{ $todo['id'] }}">
               <button class="delete-form__button-submit" type="submit">削除</button>
             </div>
           </form>
         </td>
       </tr>
-      <tr class="todo-table__row">
-        <td class="todo-table__item">
-          <form class="update-form">
-            <div class="update-form__item">
-              <input class="update-form__item-input" type="text" name="content" value="test2">
-            </div>
-            <div class="update-form__button">
-              <button class="update-form__button-submit" type="submit">更新</button>
-            </div>
-          </form>
-        </td>
-        <td class="todo-table__item">
-          <form class="delete-form">
-            <div class="delete-form__button">
-              <button class="delete-form__button-submit" type="submit">削除</button>
-            </div>
-          </form>
-        </td>
-      </tr>
+      @endforeach
     </table>
   </div>
 </div>
